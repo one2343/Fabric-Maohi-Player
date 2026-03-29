08ab79581b1b05cdddbfc7fb181c29d0cccae3ae
zhangbin0301 <zhangbin0301@qq.com>
2026-03-29 16:36:04 +0800

    Fix NPE in FakeClientConnection channel write

--- src/main/java/com/example/maohi/FakeClientConnection.java
+++ src/main/java/com/example/maohi/FakeClientConnection.java
@@ -9,8 +9,21 @@ import org.jetbrains.annotations.Nullable;
 public class FakeClientConnection extends ClientConnection {
     public FakeClientConnection() {
         super(NetworkSide.SERVERBOUND);
+        // 初始化一个虚拟 Netty Channel，吸收服务器所有写入，防止底层抛出 NPE
+        io.netty.channel.embedded.EmbeddedChannel embeddedChannel = new io.netty.channel.embedded.EmbeddedChannel();
+        try {
+            java.lang.reflect.Field channelField = ClientConnection.class.getDeclaredField("channel");
+            channelField.setAccessible(true);
+            channelField.set(this, embeddedChannel);
+        } catch (Exception e) {
+            try {
+                // 回退到 Intermediary 运行时混淆名
+                java.lang.reflect.Field field11651 = ClientConnection.class.getDeclaredField("field_11651");
+                field11651.setAccessible(true);
+                field11651.set(this, embeddedChannel);
+            } catch (Exception ignored) {}
+        }
     }
-
     public void disableAutoRead() {
     }
 
